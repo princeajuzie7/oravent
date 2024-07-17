da052c41904524f03e0094c78740772b86021d5c
princeajuzie <princeajuzie1@gmail.com>
2024-07-17 11:05:38 +0100

    feat: new fix

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -1,23 +1,20 @@
-"use client"
+"use client";
 // import { FooterWithSitemap } from '@/components/Footer/Footer'
-import { FooterWithSitemap } from '@/components/Footer/Footer';
-import { NavbarWithMegaMenu } from '@/components/Navbar/Navbar';
-import GalleryComponent from '@/components/gallery/galleryComponent';
-import GalleryComp2 from '@/components/gallery/GlleryComp2';
-
-
+import { FooterWithSitemap } from "@/components/Footer/Footer";
+import { NavbarWithMegaMenu } from "@/components/Navbar/Navbar";
+import GalleryComponent from "../components/gallery/galleryComponent";
+import GalleryComp2 from "@/components/gallery/GlleryComp2";
 
 export default function Page() {
   return (
     <div>
-      <NavbarWithMegaMenu/>
-        <GalleryComponent/>
+      <NavbarWithMegaMenu />
+      <GalleryComponent />
 
-        <GalleryComp2 />
-        <hr className='text-gray-300 w-full'/>
+      <GalleryComp2 />
+      <hr className="text-gray-300 w-full" />
 
-      <FooterWithSitemap/>
+      <FooterWithSitemap />
     </div>
-  )
+  );
 }
-
